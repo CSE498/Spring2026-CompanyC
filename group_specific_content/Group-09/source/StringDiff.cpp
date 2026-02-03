cf85363053464a4723dd11b364899b8394aa4105
Andrew Shilman <Andrew.Shilman@gmail.com>
2026-02-03 17:10:24 -0500

    Implemented the MakeDiff function with the prefix and suffix detections

--- group_specific_content/Group-09/source/StringDiff.cpp
+++ group_specific_content/Group-09/source/StringDiff.cpp
@@ -19,6 +19,44 @@ std::size_t StringDiff::ComputeHash(const std::string& str) {
 
 
 
+StringDiff::Diff StringDiff::MakeDiff(const std::string& base, const std::string& updated) {
+    Diff patch;
+
+    // signature info
+    patch.base_hash = ComputeHash(base);
+    patch.base_length = base.length();
+
+    // prefix length
+    std::size_t prefix_length = 0;
+    while (prefix_length < base.length() && prefix_length < updated.length() && base[prefix_length] == updated[prefix_length]) {
+        prefix_length++;
+    }
+
+    patch.prefix_length = prefix_length;
+
+
+    // suffix length
+    std::size_t suffix_length = 0;
+    std::size_t base_remaining = base.length() - prefix_length;
+    std::size_t updated_remaining = updated.length() - prefix_length;
+
+    while (suffix_length < base_remaining && suffix_length < updated_remaining && base[base.length() - 1 - suffix_length] == updated[updated.length() - 1 - suffix_length]) {
+        suffix_length++;
+    }
+
+    patch.suffix_length = suffix_length;
+
+
+    //get new middle part
+    std::size_t start_pos = prefix_length;
+    std::size_t end_pos = updated.length() - suffix_length;
+    std::size_t middle_length = end_pos - start_pos;
+
+    patch.replacement = updated.substr(start_pos, middle_length);
+    return patch;
+}
+
+
 
 
 
