3c27e85a55e4a4e85b89bb933c92cccb25dbcde1
John Korreck <johnkorreck3@gmail.com>
2026-02-11 21:24:13 -0500

    made improvements to StateGrid class

--- source/Worlds/DynamicWorld/StateGrid.hpp
+++ source/Worlds/DynamicWorld/StateGrid.hpp
@@ -5,7 +5,8 @@
 class StateGrid {
 public:
     struct StateGridPosition {
-
+      size_t X;
+      size_t Y;
     };
 
     struct State {
@@ -13,17 +14,46 @@ public:
     };
 
     // Custom Constructor
-    StateGrid(int width, int height);
+    StateGrid(int height, int width)
+      : mGrid(height*width) {
+    }
+
+    // -- Accessors --
+
+    size_t GetHeight() const { return mHeight; }
+    size_t GetWidth() const { return mWidth; }
 
     // Public Member Functions
-    State GetState(StateGridPosition pos);
-    void SetState(StateGridPosition pos, State stateID);
-    bool InBounds(StateGridPosition pos);
+    State const &GetState(StateGridPosition pos) { 
+      return mGrid[ToIndex(pos.X, pos.Y)];
+    };
+    
+    void SetState(StateGridPosition pos, State state) {
+      mGrid[ToIndex(pos.X, pos.Y)] = state;
+    };
+    
+    bool InBounds(StateGridPosition pos) {
+      return pos.X < mWidth && pos.Y < mHeight;
+    };
 
 private:
+    // Dimensions
+    size_t mHeight;
+    size_t mWidth;
+
     // Main data structure to manage states
-    std::vector<std::vector<State> > mGrid;
-        
-    // Resize the current grid without resetting any filled positions
-    void Resize(int width, int height);
+    std::vector<State> mGrid;
+    
+    // Converts x and y coordinates to position in the grid
+    size_t const ToIndex(size_t x, size_t y) {
+      return x + y * mWidth;
+    };
+
+    // Resize the current grid (Unsure of how to implement)
+    void Resize(int newHeight, int newWidth) {
+      // Create new grid with new sizing
+      std::vector<State> newGrid(newHeight*newWidth);
+
+      // Copy old elements into new grid instance
+    }
 };
