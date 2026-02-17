f4c87b4d8d91ff96fd3de24427963712dde45f39
joshyt100 <joshyt100@gmail.com>
2026-02-16 21:21:36 -0500

    Added headers for std::sort and assert for ActionMap

--- group_specific_content/Group-06/Classes/ActionMap.cpp
+++ group_specific_content/Group-06/Classes/ActionMap.cpp
@@ -1,98 +1,84 @@
 #include "ActionMap.h"
+#include <algorithm>
 #include <assert.h>
 
+bool ActionMap::AddFunction(const std::string &name,
+                            std::function<void()> func) {
 
-bool ActionMap::AddFunction(const std::string& name, std::function<void()> func) {
-    
-    assert(!name.empty());
-    if (HasFunction(name)) {
-        return false;
-    }
-    function_map.emplace(
-        name,
-        FunctionEntry(
-            std::any(std::move(func)),
-            std::type_index(typeid(std::function<void()>))
-        )
-    );
-    return true;
+  assert(!name.empty());
+  if (HasFunction(name)) {
+    return false;
+  }
+  function_map.emplace(
+      name, FunctionEntry(std::any(std::move(func)),
+                          std::type_index(typeid(std::function<void()>))));
+  return true;
 }
 
-void ActionMap::ReplaceFunction(const std::string& name, std::function<void()> func) {
+void ActionMap::ReplaceFunction(const std::string &name,
+                                std::function<void()> func) {
 
-    assert(!name.empty());    
-    function_map.erase(name);
-    function_map.emplace(
-        name,
-        FunctionEntry(
-            std::any(std::move(func)),
-            std::type_index(typeid(std::function<void()>))
-        )
-    );
+  assert(!name.empty());
+  function_map.erase(name);
+  function_map.emplace(
+      name, FunctionEntry(std::any(std::move(func)),
+                          std::type_index(typeid(std::function<void()>))));
 }
 
-// Ngl I chatted this one, will need to rewrite it to better understand passing the return of the function to where it needs to go.
-std::optional<std::string> ActionMap::Trigger(const std::string &name)
-{
-    // Find the function
-    auto it = function_map.find(name);
-    if (it == function_map.end()) {
-        return "Function '" + name + "' not found in ActionMap";
-    }
-    
-    // Verify type matches (zero-argument function)
-    std::type_index expected_type = std::type_index(typeid(std::function<void()>));
-    if (it->second.type_info != expected_type) {
-        return "Type mismatch for function '" + name + "': expected zero arguments";
-    }
-    
-    // Extract and call the function
-    try {
-        auto& func = std::any_cast<std::function<void()>&>(it->second.function);
-        func();
-        return std::nullopt; // Success
-    } catch (const std::bad_any_cast&) {
-        return "Internal error: failed to cast function '" + name + "'";
-    } catch (const std::exception& e) {
-        return "Exception thrown by function '" + name + "': " + e.what();
-    }
-    return std::optional<std::string>();
+// Ngl I chatted this one, will need to rewrite it to better understand passing
+// the return of the function to where it needs to go.
+std::optional<std::string> ActionMap::Trigger(const std::string &name) {
+  // Find the function
+  auto it = function_map.find(name);
+  if (it == function_map.end()) {
+    return "Function '" + name + "' not found in ActionMap";
+  }
+
+  // Verify type matches (zero-argument function)
+  std::type_index expected_type =
+      std::type_index(typeid(std::function<void()>));
+  if (it->second.type_info != expected_type) {
+    return "Type mismatch for function '" + name + "': expected zero arguments";
+  }
+
+  // Extract and call the function
+  try {
+    auto &func = std::any_cast<std::function<void()> &>(it->second.function);
+    func();
+    return std::nullopt; // Success
+  } catch (const std::bad_any_cast &) {
+    return "Internal error: failed to cast function '" + name + "'";
+  } catch (const std::exception &e) {
+    return "Exception thrown by function '" + name + "': " + e.what();
+  }
+  return std::optional<std::string>();
 }
 
-bool ActionMap::RemoveFunction(const std::string &name)
-{
-    auto it = function_map.find(name);
-    if (it == function_map.end()) {
-        return false;
-    }
-    function_map.erase(it);
-    return true;
+bool ActionMap::RemoveFunction(const std::string &name) {
+  auto it = function_map.find(name);
+  if (it == function_map.end()) {
+    return false;
+  }
+  function_map.erase(it);
+  return true;
 }
 
-bool ActionMap::HasFunction(const std::string &name) const
-{
-    return (function_map.find(name) != function_map.end());
+bool ActionMap::HasFunction(const std::string &name) const {
+  return (function_map.find(name) != function_map.end());
 }
 
-std::vector<std::string> ActionMap::GetFunctionNames() const
-{
-    std::vector<std::string> names;
-    names.reserve(function_map.size());
-    
-    for (const auto& pair : function_map) {
-        names.push_back(pair.first);
-    }
-    std::sort(names.begin(), names.end());
-    
-    return names;
+std::vector<std::string> ActionMap::GetFunctionNames() const {
+  std::vector<std::string> names;
+  names.reserve(function_map.size());
+
+  for (const auto &pair : function_map) {
+    names.push_back(pair.first);
+  }
+  std::sort(names.begin(), names.end());
+
+  return names;
 }
 
-void ActionMap::Clear()
-{
-    function_map.clear();
-}
+void ActionMap::Clear() { function_map.clear(); }
 
-size_t ActionMap::Count() const
-{
-    return function_map.size();
-}
+size_t ActionMap::Count() const { return function_map.size(); }
