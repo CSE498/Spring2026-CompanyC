1c3f7cf0f3caeed9db22c146be54844140ffb9fe
Fighterplayer13 <151691364+Fighterplayer13@users.noreply.github.com>
2026-02-16 23:09:35 -0500

    Change AddTags parameter to const reference

--- source/tools/AnnotationSet.hpp
+++ source/tools/AnnotationSet.hpp
@@ -12,7 +12,7 @@ class AnnotationSet{
         AnnotationSet()=delete;
         AnnotationSet(int obj, std::set<std::string> tags={});
         void AddTag(std::string);
-        void AddTags(std::vector<std::string>);
+        void AddTags(const std::vector<std::string>);
         bool RemoveTags(std::vector<std::string>);
         bool RemoveTag(std::string);
         bool FindTag(std::string);
