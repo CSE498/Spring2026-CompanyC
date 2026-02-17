417e0cd297ad030584607198c9a7b91f5dadbec4
Fighterplayer13 <151691364+Fighterplayer13@users.noreply.github.com>
2026-02-16 22:28:22 -0500

    Added AnnotationSet .cpp and .hpp

---
+++ source/tools/AnnotationSet.cpp
@@ -0,0 +1,113 @@
+//By Sachin Karatha
+//
+//This class stores tags that are attached to a given object with a unique Id.
+//
+//Class includes methods to add and remove tags using vectors or strings depending
+//on the amount being added. Tags can also be checked for membership whether it be
+//only one or multiple. 
+
+#include "../../source/tools/AnnotationSet.hpp"
+#include <set>
+#include <string>
+
+
+//new constructor initializes object Id and tags with a default value of {}
+AnnotationSet::AnnotationSet(int obj, std::set<std::string> inTags){
+    objectId = obj;
+    tags = inTags;
+}
+
+
+//adds a tag to the tags
+void AnnotationSet::AddTag(std::string tag){
+    tag.erase(std::remove_if(tag.begin(), tag.end(), [](unsigned char x){ return std::isspace(x); }), tag.end());
+    if (!tag.empty()){
+        tags.insert(tag);
+    }
+}
+
+//adds tags to the set of tags
+void AnnotationSet::AddTags(std::vector<std::string> addedTags){
+    for(int i=0;i<addedTags.size();i++){
+        std::string tag=addedTags[i];
+        tag.erase(std::remove_if(tag.begin(), tag.end(), [](unsigned char x){ return std::isspace(x); }), tag.end());
+        if (!tag.empty()){
+            tags.insert(addedTags[i]);
+        }
+    }
+}
+
+//removes tags from the set of tags
+//returns true if all were successfull false otherwise
+bool AnnotationSet::RemoveTags(std::vector<std::string> removedTags){
+    bool removed=true;
+    for(int i=0;i<removedTags.size();i++){
+        bool rem=tags.erase(removedTags[i]);
+        if(rem==false){
+            removed=false;
+        }
+
+    }
+    return removed;
+}
+
+//removes a tag from the set of tags
+//returns true if successfull false otherwise
+bool AnnotationSet::RemoveTag(std::string tag){
+    return tags.erase(tag);
+}
+
+//checks if a certain tag is attached to object
+//returns true if successfull false otherwise
+bool AnnotationSet::FindTag(std::string tag){
+    if(tags.count(tag)==1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+//checks if any of a given set of tags are attached to an object
+//returns true if successfull false otherwise
+bool AnnotationSet::FindAnyTag(std::vector<std::string> searchTags){
+    for(int i=0;i<searchTags.size();i++){
+        std::string currSearched=searchTags[i];
+        if (tags.count(currSearched)==1){
+            return true;
+        }
+    }
+    return false;
+}
+//checks if all of a given set of tags are attached to an object
+//returns true if successfull false otherwise
+bool AnnotationSet::FindAllTags(std::vector<std::string> searchTags){
+    for(int i=0;i<searchTags.size();i++){
+        std::string currSearched=searchTags[i];
+        if (tags.count(currSearched)==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+//deletes all tags in the set without deleting the object
+void AnnotationSet::DeleteAllTags(){
+    tags.clear();
+}
+
+//gets object Id
+//returns int of object id
+int AnnotationSet::GetObjId(){
+    return objectId;
+}
+
+//gets tags
+//returns the set containing all tags
+std::set<std::string> AnnotationSet::GetTags(){
+    return tags;
+}
+
+int AnnotationSet::Size(){
+    return tags.size();
+}
---
+++ source/tools/AnnotationSet.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <set>
+class AnnotationSet{
+    private:
+        int objectId; //id of attached object
+        std::set<std::string> tags; //collection of all tags
+    public:
+        //default constructor deleted
+        AnnotationSet()=delete;
+        AnnotationSet(int obj, std::set<std::string> tags={});
+        void AddTag(std::string);
+        void AddTags(std::vector<std::string>);
+        bool RemoveTags(std::vector<std::string>);
+        bool RemoveTag(std::string);
+        bool FindTag(std::string);
+        bool FindAnyTag(std::vector<std::string>);
+        bool FindAllTags(std::vector<std::string>);
+        void DeleteAllTags();
+        int GetObjId();
+        int Size();
+        std::set<std::string> GetTags();
+};
