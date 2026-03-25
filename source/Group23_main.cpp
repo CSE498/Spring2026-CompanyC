c68ddae4d0d007ebc25aa4c85bed8cdf8bd56731
CollinMassmann <massman1@msu.edu>
2026-03-25 16:03:56 -0400

    Initial Group23 main file

---
+++ source/Group23_main.cpp
@@ -0,0 +1,32 @@
+/**
+ * This file is part of the Fall 2026, CSE 498, section 2, course project.
+ * @brief An example driver for Group 23 module
+ **/
+
+#include "Agents/PacingAgent.hpp"
+#include "Worlds/MazeWorld.hpp"
+#include "Interfaces/TrashInterface.hpp"
+#include "tools/DataLog.hpp"
+#include "tools/ReplayDriver.hpp"
+#include "tools/OutputManager.hpp"
+
+using namespace cse498;
+
+// Timer and ActionLog classes are implemented within the WorldBase.hpp
+
+int main()
+{
+  MazeWorld world;
+  world.AddAgent<PacingAgent>("Pacer 1").SetLocation(WorldPosition{3,1});
+  world.AddAgent<PacingAgent>("Pacer 2").SetLocation(WorldPosition{6,1});
+  world.AddAgent<PacingAgent>("Guard 1").SetHorizontal().SetLocation(WorldPosition{7,7});
+  world.AddAgent<PacingAgent>("Guard 2").SetHorizontal().ToggleDirection().SetLocation(WorldPosition{8,8});
+  world.AddAgent<TrashInterface>("Interface").SetSymbol('@').SetLocation(WorldPosition{1,1});
+
+  // Datalog
+
+  // Datalog
+
+  world.Run();
+  
+}
